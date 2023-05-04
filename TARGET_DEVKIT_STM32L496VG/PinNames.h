28470188796e1eea0599d1086df1afc24d0e89d9
wdx04 <wdx04@outlook.com>
2023-05-04 13:48:16 +0800

    initial commit

---
+++ TARGET_DEVKIT_STM32L496VG/PinNames.h
@@ -0,0 +1,234 @@
+/* mbed Microcontroller Library
+ *******************************************************************************
+ * Copyright (c) 2016, STMicroelectronics
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 3. Neither the name of STMicroelectronics nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *******************************************************************************
+ */
+
+/* MBED TARGET LIST: MTS_DRAGONFLY_L471QG */
+
+#ifndef MBED_PINNAMES_H
+#define MBED_PINNAMES_H
+
+#include "cmsis.h"
+#include "PinNamesTypes.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef enum {
+    ALT0  = 0x100,
+    ALT1  = 0x200,
+    ALT2  = 0x300,
+} ALTx;
+
+typedef enum {
+    PA_0       = 0x00,
+    PA_0_ALT0  = PA_0  | ALT0, // same pin used for alternate HW
+    PA_1       = 0x01,
+    PA_1_ALT0  = PA_1  | ALT0, // same pin used for alternate HW
+    PA_2       = 0x02,
+    PA_2_ALT0  = PA_2  | ALT0, // same pin used for alternate HW
+    PA_3       = 0x03,
+    PA_3_ALT0  = PA_3  | ALT0, // same pin used for alternate HW
+    PA_4       = 0x04,
+    PA_4_ALT0  = PA_4  | ALT0, // same pin used for alternate HW
+    PA_5       = 0x05,
+    PA_5_ALT0  = PA_5  | ALT0, // same pin used for alternate HW
+    PA_6       = 0x06,
+    PA_6_ALT0  = PA_6  | ALT0, // same pin used for alternate HW
+    PA_7       = 0x07,
+    PA_7_ALT0  = PA_7  | ALT0, // same pin used for alternate HW
+    PA_7_ALT1  = PA_7  | ALT1, // same pin used for alternate HW
+    PA_7_ALT2  = PA_7  | ALT2, // same pin used for alternate HW
+    PA_8       = 0x08,
+    PA_9       = 0x09,
+    PA_10      = 0x0A,
+    PA_11      = 0x0B,
+    PA_12      = 0x0C,
+    PA_13      = 0x0D,
+    PA_14      = 0x0E,
+    PA_15      = 0x0F,
+    PA_15_ALT0 = PA_15 | ALT0, // same pin used for alternate HW
+
+    PB_0       = 0x10,
+    PB_0_ALT0  = PB_0  | ALT0, // same pin used for alternate HW
+    PB_0_ALT1  = PB_0  | ALT1, // same pin used for alternate HW
+    PB_1       = 0x11,
+    PB_1_ALT0  = PB_1  | ALT0, // same pin used for alternate HW
+    PB_1_ALT1  = PB_1  | ALT1, // same pin used for alternate HW
+    PB_2       = 0x12,
+    PB_3       = 0x13,
+    PB_3_ALT0  = PB_3  | ALT0, // same pin used for alternate HW
+    PB_4       = 0x14,
+    PB_4_ALT0  = PB_4  | ALT0, // same pin used for alternate HW
+    PB_5       = 0x15,
+    PB_5_ALT0  = PB_5  | ALT0, // same pin used for alternate HW
+    PB_6       = 0x16,
+    PB_6_ALT0  = PB_6  | ALT0, // same pin used for alternate HW
+    PB_7       = 0x17,
+    PB_7_ALT0  = PB_7  | ALT0, // same pin used for alternate HW
+    PB_8       = 0x18,
+    PB_8_ALT0  = PB_8  | ALT0, // same pin used for alternate HW
+    PB_9       = 0x19,
+    PB_9_ALT0  = PB_9  | ALT0, // same pin used for alternate HW
+    PB_10      = 0x1A,
+    PB_10_ALT0 = PB_10 | ALT0, // same pin used for alternate HW
+    PB_11      = 0x1B,
+    PB_11_ALT0 = PB_11 | ALT0, // same pin used for alternate HW
+    PB_12      = 0x1C,
+    PB_13      = 0x1D,
+    PB_13_ALT0 = PB_13 | ALT0, // same pin used for alternate HW
+    PB_14      = 0x1E,
+    PB_14_ALT0 = PB_14 | ALT0, // same pin used for alternate HW
+    PB_14_ALT1 = PB_14 | ALT1, // same pin used for alternate HW
+    PB_15      = 0x1F,
+    PB_15_ALT0 = PB_15 | ALT0, // same pin used for alternate HW
+    PB_15_ALT1 = PB_15 | ALT1, // same pin used for alternate HW
+
+    PC_0       = 0x20,
+    PC_0_ALT0  = PC_0  | ALT0, // same pin used for alternate HW
+    PC_0_ALT1  = PC_0  | ALT1, // same pin used for alternate HW
+    PC_1       = 0x21,
+    PC_1_ALT0  = PC_1  | ALT0, // same pin used for alternate HW
+    PC_1_ALT1  = PC_1  | ALT1, // same pin used for alternate HW
+    PC_2       = 0x22,
+    PC_2_ALT0  = PC_2  | ALT0, // same pin used for alternate HW
+    PC_2_ALT1  = PC_2  | ALT1, // same pin used for alternate HW
+    PC_3       = 0x23,
+    PC_3_ALT0  = PC_3  | ALT0, // same pin used for alternate HW
+    PC_3_ALT1  = PC_3  | ALT1, // same pin used for alternate HW
+    PC_4       = 0x24,
+    PC_4_ALT0  = PC_4  | ALT0, // same pin used for alternate HW
+    PC_5       = 0x25,
+    PC_5_ALT0  = PC_5  | ALT0, // same pin used for alternate HW
+    PC_6       = 0x26,
+    PC_6_ALT0  = PC_6  | ALT0, // same pin used for alternate HW
+    PC_7       = 0x27,
+    PC_7_ALT0  = PC_7  | ALT0, // same pin used for alternate HW
+    PC_8       = 0x28,
+    PC_8_ALT0  = PC_8  | ALT0, // same pin used for alternate HW
+    PC_9       = 0x29,
+    PC_9_ALT0  = PC_9  | ALT0, // same pin used for alternate HW
+    PC_10      = 0x2A,
+    PC_10_ALT0 = PC_10 | ALT0, // same pin used for alternate HW
+    PC_11      = 0x2B,
+    PC_11_ALT0 = PC_11 | ALT0, // same pin used for alternate HW
+    PC_12 = 0x2C,
+    PC_13 = 0x2D,
+    PC_14 = 0x2E,
+    PC_15 = 0x2F,
+
+    PD_0 = 0x30,
+    PD_1 = 0x31,
+    PD_2 = 0x32,
+    PD_3 = 0x33,
+    PD_4 = 0x34,
+    PD_5 = 0x35,
+    PD_6 = 0x36,
+    PD_7 = 0x37,
+    PD_8 = 0x38,
+    PD_9 = 0x39,
+    PD_10 = 0x3A,
+    PD_11 = 0x3B,
+    PD_12 = 0x3C,
+    PD_13 = 0x3D,
+    PD_14 = 0x3E,
+    PD_15 = 0x3F,
+
+    PE_0 = 0x40,
+    PE_1 = 0x41,
+    PE_2 = 0x42,
+    PE_3 = 0x43,
+    PE_4 = 0x44,
+    PE_5 = 0x45,
+    PE_6 = 0x46,
+    PE_7 = 0x47,
+    PE_8 = 0x48,
+    PE_9 = 0x49,
+    PE_10 = 0x4A,
+    PE_11 = 0x4B,
+    PE_12 = 0x4C,
+    PE_13 = 0x4D,
+    PE_14 = 0x4E,
+    PE_15 = 0x4F,
+
+    PH_0 = 0x70,
+    PH_1 = 0x71,
+
+    // ADC internal channels
+    ADC_TEMP = 0xF0,
+    ADC_VREF = 0xF1,
+    ADC_VBAT = 0xF2,
+
+    // USB debug port
+    CONSOLE_TX = PB_10,
+    CONSOLE_RX = PB_11,
+    UART1_TX = PA_2,
+    UART1_RX = PA_3,
+
+    // SPI1 for LCD(ST7789H2)
+    SPI1_MOSI = PA_7,
+    LCD_DC = PA_6,
+    SPI1_SCK  = PA_5,
+    SPI1_SSEL = PA_4,
+    LCD_PWR_EN = PE_7,
+    LCD_RST = PB_2,
+
+    // I2C for sensors
+    I2C2_SCL = PB_13,
+    I2C2_SDA = PB_14,
+    I2C3_SCL = PC_0,
+    I2C3_SDA = PC_1,
+    I2C4_SCL = PD_12,
+    I2C4_SDA = PD_13,
+    	
+    // SDMMC
+    SDMMC_CMD = PD_2,
+    SDMMC_CLK = PC_12,
+    SDMMC_D0 = PC_8,
+    SDMMC_D1 = PC_9,
+    SDMMC_D2 = PC_10,
+    SDMMC_D3 = PC_11,
+    
+    LED1 = PB_6,
+    LED2 = PE_3,
+    LED3 = PD_15,
+
+    BUTTON1 = PE_11, // M¼ü
+    BUTTON2 = PE_14, // A¼ü
+    BUTTON3 = PE_10, // B¼ü
+
+    // Not connected
+    NC = (int) 0xFFFFFFFF
+} PinName;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ TARGET_STM32L431RC/sdio_device.c
@@ -0,0 +1,549 @@
+/* mbed Microcontroller Library
+ * Copyright (c) 2017 ARM Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+#include "sdio_device.h"
+#include "platform/mbed_error.h"
+
+/* Extern variables ---------------------------------------------------------*/
+
+SD_HandleTypeDef hsd;
+
+// simple flags for DMA pending signaling
+volatile uint8_t SD_DMA_ReadPendingState = SD_TRANSFER_OK;
+volatile uint8_t SD_DMA_WritePendingState = SD_TRANSFER_OK;
+
+static HAL_StatusTypeDef SD_DMAConfigRx(SD_HandleTypeDef *hsd);
+static HAL_StatusTypeDef SD_DMAConfigTx(SD_HandleTypeDef *hsd);
+
+/**
+  * @brief  Handles SD card interrupt request.
+  * @retval None
+  */
+void SDMMC1_IRQHandler(void)
+{
+   HAL_SD_IRQHandler(&hsd);
+}
+
+/**
+  * @brief  Handles SD DMA transfer interrupt request.
+  * @retval None
+  */
+void DMA2_Channel5_IRQHandler(void)
+{
+  if((hsd.Context == (SD_CONTEXT_DMA | SD_CONTEXT_READ_SINGLE_BLOCK)) ||
+     (hsd.Context == (SD_CONTEXT_DMA | SD_CONTEXT_READ_MULTIPLE_BLOCK)))
+  {
+    HAL_DMA_IRQHandler(hsd.hdmarx);
+  }
+  else if((hsd.Context == (SD_CONTEXT_DMA | SD_CONTEXT_WRITE_SINGLE_BLOCK)) ||
+          (hsd.Context == (SD_CONTEXT_DMA | SD_CONTEXT_WRITE_MULTIPLE_BLOCK)))
+  {
+    HAL_DMA_IRQHandler(hsd.hdmatx);
+  }
+}
+
+/**
+ *
+ * @param hsd:  Handle for SD handle Structure definition
+ */
+void HAL_SD_MspInit(SD_HandleTypeDef *hsd)
+{
+  GPIO_InitTypeDef gpioinitstruct = {0};
+  RCC_PeriphCLKInitTypeDef  RCC_PeriphClkInit;
+
+  HAL_RCCEx_GetPeriphCLKConfig(&RCC_PeriphClkInit);
+
+  /* Configure the SDMMC1 clock source. The clock is derived from the PLLSAI1 */
+  /* Hypothesis is that PLLSAI1 VCO input is 8Mhz */
+  RCC_PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_SDMMC1;
+  RCC_PeriphClkInit.PLLSAI1.PLLSAI1N = 24;
+  RCC_PeriphClkInit.PLLSAI1.PLLSAI1Q = 4;
+  RCC_PeriphClkInit.PLLSAI1.PLLSAI1ClockOut = RCC_PLLSAI1_48M2CLK;
+  RCC_PeriphClkInit.Sdmmc1ClockSelection = RCC_SDMMC1CLKSOURCE_PLLSAI1;
+  if (HAL_RCCEx_PeriphCLKConfig(&RCC_PeriphClkInit) != HAL_OK)
+  {
+    while (1) {}
+  }
+
+  /* Enable SDMMC1 clock */
+  __HAL_RCC_SDMMC1_CLK_ENABLE();
+
+  /* Enable DMA2 clocks */
+  __HAL_RCC_DMA2_CLK_ENABLE();
+
+  /* Enable GPIOs clock */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOD_CLK_ENABLE();
+
+  /* Common GPIO configuration */
+  gpioinitstruct.Mode      = GPIO_MODE_AF_PP;
+  gpioinitstruct.Pull      = GPIO_NOPULL;
+  gpioinitstruct.Speed     = GPIO_SPEED_FREQ_VERY_HIGH;
+  gpioinitstruct.Alternate = GPIO_AF12_SDMMC1;
+
+  /* GPIOC configuration */
+  gpioinitstruct.Pin = GPIO_PIN_8 | GPIO_PIN_9 | GPIO_PIN_10 | GPIO_PIN_11 | GPIO_PIN_12;
+
+  HAL_GPIO_Init(GPIOC, &gpioinitstruct);
+
+  /* GPIOD configuration */
+  gpioinitstruct.Pin = GPIO_PIN_2;
+  HAL_GPIO_Init(GPIOD, &gpioinitstruct);
+
+  /* NVIC configuration for SDMMC1 interrupts */
+  HAL_NVIC_SetPriority(SDMMC1_IRQn, 5, 0);
+  HAL_NVIC_EnableIRQ(SDMMC1_IRQn);
+
+  /* DMA initialization should be done here but , as there is only one channel for RX and TX it is configured and done directly when required*/
+}
+
+/**
+ *
+ * @param hsd:  Handle for SD handle Structure definition
+ */
+void HAL_SD_MspDeInit(SD_HandleTypeDef *hsd)
+{
+  GPIO_InitTypeDef gpioinitstruct = {0};
+
+  /* Enable SDMMC1 clock */
+  __HAL_RCC_SDMMC1_CLK_DISABLE();
+
+  /* Enable DMA2 clocks */
+  __HAL_RCC_DMA2_CLK_DISABLE();
+
+  /* Enable GPIOs clock */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOD_CLK_ENABLE();
+
+  /* Common GPIO configuration */
+  gpioinitstruct.Mode      = GPIO_MODE_ANALOG;
+  gpioinitstruct.Pull      = GPIO_NOPULL;
+  gpioinitstruct.Speed     = GPIO_SPEED_FREQ_LOW;
+  gpioinitstruct.Alternate = 0;
+
+  /* GPIOC configuration */
+  gpioinitstruct.Pin = GPIO_PIN_8 | GPIO_PIN_9 | GPIO_PIN_10 | GPIO_PIN_11 | GPIO_PIN_12;
+
+  HAL_GPIO_Init(GPIOC, &gpioinitstruct);
+
+  /* GPIOD configuration */
+  gpioinitstruct.Pin = GPIO_PIN_2;
+  HAL_GPIO_Init(GPIOD, &gpioinitstruct);
+
+  /* NVIC configuration for SDMMC1 interrupts */
+  HAL_NVIC_DisableIRQ(SDMMC1_IRQn);
+}
+
+/**
+ * @brief  DeInitializes the SD MSP.
+ * @param  hsd: SD handle
+ * @param  Params : pointer on additional configuration parameters, can be NULL.
+ */
+void SD_MspDeInit(SD_HandleTypeDef *hsd, void *Params)
+{
+
+#if 0
+    if (hsd->Instance == SDMMC2) {
+    }
+#endif
+}
+
+
+
+/**
+ * @brief  Initializes the SD card device.
+ * @retval SD status
+ */
+uint8_t SD_Init(void)
+{
+  uint8_t sd_state = MSD_OK;
+
+  /* uSD device interface configuration */
+  hsd.Instance = SDMMC1;
+  hsd.Init.ClockEdge           = SDMMC_CLOCK_EDGE_RISING;
+  hsd.Init.ClockBypass         = SDMMC_CLOCK_BYPASS_DISABLE;
+  hsd.Init.ClockPowerSave      = SDMMC_CLOCK_POWER_SAVE_DISABLE;
+  hsd.Init.BusWide             = SDMMC_BUS_WIDE_1B;
+  hsd.Init.HardwareFlowControl = SDMMC_HARDWARE_FLOW_CONTROL_ENABLE;
+  hsd.Init.ClockDiv            = SDMMC_TRANSFER_CLK_DIV;
+
+  /* HAL SD initialization */
+  if (HAL_SD_Init(&hsd) != HAL_OK)
+  {
+    sd_state = MSD_ERROR;
+  }
+
+  /* Configure SD Bus width */
+  if (sd_state == MSD_OK)
+  {
+    /* Enable wide operation */
+    if (HAL_SD_ConfigWideBusOperation(&hsd, SDMMC_BUS_WIDE_4B) != HAL_OK)
+    {
+      sd_state = MSD_ERROR;
+    }
+    else
+    {
+      sd_state = MSD_OK;
+    }
+  }
+
+  return  sd_state;
+}
+
+/**
+ * @brief  DeInitializes the SD card device.
+ * @retval SD status
+ */
+uint8_t SD_DeInit(void)
+{
+  uint8_t sd_state = MSD_OK;
+
+  hsd.Instance = SDMMC1;
+  /* HAL SD deinitialization */
+  if (HAL_SD_DeInit(&hsd) != HAL_OK)
+  {
+    sd_state = MSD_ERROR;
+  }
+
+  return  sd_state;
+}
+
+/**
+ * @brief  Reads block(s) from a specified address in an SD card, in polling mode.
+ * @param  pData: Pointer to the buffer that will contain the data to transmit
+ * @param  ReadAddr: Address from where data is to be read
+ * @param  NumOfBlocks: Number of SD blocks to read
+ * @param  Timeout: Timeout for read operation
+ * @retval SD status
+ */
+uint8_t SD_ReadBlocks(uint32_t *pData, uint32_t ReadAddr, uint32_t NumOfBlocks, uint32_t Timeout)
+{
+    uint8_t sd_state = MSD_OK;
+
+    int result_result = HAL_SD_ReadBlocks(&hsd, (uint8_t *)pData, ReadAddr, NumOfBlocks, Timeout);
+    if (result_result != HAL_OK)
+    {
+        sd_state = MSD_ERROR;
+    }
+
+    return sd_state;
+}
+
+/**
+ * @brief  Writes block(s) to a specified address in an SD card, in polling mode.
+ * @param  pData: Pointer to the buffer that will contain the data to transmit
+ * @param  WriteAddr: Address from where data is to be written
+ * @param  NumOfBlocks: Number of SD blocks to write
+ * @param  Timeout: Timeout for write operation
+ * @retval SD status
+ */
+uint8_t SD_WriteBlocks(uint32_t *pData, uint32_t WriteAddr, uint32_t NumOfBlocks, uint32_t Timeout)
+{
+    uint8_t sd_state = MSD_OK;
+
+    if (HAL_SD_WriteBlocks(&hsd, (uint8_t *)pData, WriteAddr, NumOfBlocks, Timeout) != HAL_OK)
+    {
+        sd_state = MSD_ERROR;
+    }
+
+    return sd_state;
+}
+
+/**
+ * @brief  Reads block(s) from a specified address in an SD card, in DMA mode.
+ * @param  pData: Pointer to the buffer that will contain the data to transmit
+ * @param  ReadAddr: Address from where data is to be read
+ * @param  NumOfBlocks: Number of SD blocks to read
+ * @retval SD status
+ */
+uint8_t SD_ReadBlocks_DMA(uint32_t *pData, uint32_t ReadAddr, uint32_t NumOfBlocks)
+{
+  HAL_StatusTypeDef  sd_state = HAL_OK;
+
+  SD_DMA_ReadPendingState = SD_TRANSFER_BUSY;
+
+  /* Invalidate the dma tx handle*/
+  hsd.hdmatx = NULL;
+
+  /* Prepare the dma channel for a read operation */
+  sd_state = SD_DMAConfigRx(&hsd);
+
+  if (sd_state == HAL_OK)
+  {
+    /* Read block(s) in DMA transfer mode */
+    sd_state = HAL_SD_ReadBlocks_DMA(&hsd, (uint8_t *)pData, ReadAddr, NumOfBlocks);
+  }
+
+  if (sd_state == HAL_OK)
+  {
+    return MSD_OK;
+  }
+  else
+  {
+    return MSD_ERROR;
+  }
+}
+
+/**
+ * @brief  Writes block(s) to a specified address in an SD card, in DMA mode.
+ * @param  pData: Pointer to the buffer that will contain the data to transmit
+ * @param  WriteAddr: Address from where data is to be written
+ * @param  NumOfBlocks: Number of SD blocks to write
+ * @retval SD status
+ */
+uint8_t SD_WriteBlocks_DMA(uint32_t *pData, uint32_t WriteAddr, uint32_t NumOfBlocks)
+{
+  HAL_StatusTypeDef  sd_state = HAL_OK;
+
+  SD_DMA_WritePendingState = SD_TRANSFER_BUSY;
+
+  /* Invalidate the dma rx handle*/
+  hsd.hdmarx = NULL;
+
+  /* Prepare the dma channel for a read operation */
+  sd_state = SD_DMAConfigTx(&hsd);
+
+  if (sd_state == HAL_OK)
+  {
+    /* Write block(s) in DMA transfer mode */
+    sd_state = HAL_SD_WriteBlocks_DMA(&hsd, (uint8_t *)pData, WriteAddr, NumOfBlocks);
+  }
+
+  if (sd_state == HAL_OK)
+  {
+    return MSD_OK;
+  }
+  else
+  {
+    return MSD_ERROR;
+  }
+}
+
+/**
+ * @brief  Erases the specified memory area of the given SD card.
+ * @param  StartAddr: Start byte address
+ * @param  EndAddr: End byte address
+ * @retval SD status
+ */
+uint8_t SD_Erase(uint32_t StartAddr, uint32_t EndAddr)
+{
+  HAL_StatusTypeDef  sd_state = HAL_OK;
+
+  sd_state = HAL_SD_Erase(&hsd, StartAddr, EndAddr);
+
+  if (sd_state == HAL_OK)
+  {
+    return MSD_OK;
+  }
+  else
+  {
+    return MSD_ERROR;
+  }
+}
+
+/**
+ * @brief  Gets the current SD card data status.
+ * @param  None
+ * @retval Data transfer state.
+ *          This value can be one of the following values:
+ *            @arg  SD_TRANSFER_OK: No data transfer is acting
+ *            @arg  SD_TRANSFER_BUSY: Data transfer is acting
+ */
+uint8_t SD_GetCardState(void)
+{
+  HAL_SD_CardStateTypedef card_state;
+  card_state = HAL_SD_GetCardState(&hsd);
+
+  if (card_state == HAL_SD_CARD_TRANSFER)
+  {
+    return (SD_TRANSFER_OK);
+  }
+  else if ((card_state == HAL_SD_CARD_SENDING) ||
+           (card_state == HAL_SD_CARD_RECEIVING) ||
+           (card_state == HAL_SD_CARD_PROGRAMMING))
+  {
+    return (SD_TRANSFER_BUSY);
+  }
+  else
+  {
+    return (SD_TRANSFER_ERROR);
+  }
+}
+
+/**
+ * @brief  Get SD information about specific SD card.
+ * @param  CardInfo: Pointer to HAL_SD_CardInfoTypedef structure
+ * @retval None
+ */
+void SD_GetCardInfo(SD_Cardinfo_t *CardInfo)
+{
+    /* Get SD card Information, copy structure for portability */
+    HAL_SD_CardInfoTypeDef HAL_CardInfo;
+
+    HAL_SD_GetCardInfo(&hsd, &HAL_CardInfo);
+
+    if (CardInfo)
+    {
+        CardInfo->CardType = HAL_CardInfo.CardType;
+        CardInfo->CardVersion = HAL_CardInfo.CardVersion;
+        CardInfo->Class = HAL_CardInfo.Class;
+        CardInfo->RelCardAdd = HAL_CardInfo.RelCardAdd;
+        CardInfo->BlockNbr = HAL_CardInfo.BlockNbr;
+        CardInfo->BlockSize = HAL_CardInfo.BlockSize;
+        CardInfo->LogBlockNbr = HAL_CardInfo.LogBlockNbr;
+        CardInfo->LogBlockSize = HAL_CardInfo.LogBlockSize;
+    }
+}
+
+/**
+ * @brief  Check if a DMA operation is pending
+ * @retval DMA operation is pending
+ *          This value can be one of the following values:
+ *            @arg  SD_TRANSFER_OK: No data transfer is acting
+ *            @arg  SD_TRANSFER_BUSY: Data transfer is acting
+ */
+uint8_t SD_DMA_ReadPending(void)
+{
+    return SD_DMA_ReadPendingState;
+}
+
+/**
+ * @brief  Check if a DMA operation is pending
+ * @retval DMA operation is pending
+ *          This value can be one of the following values:
+ *            @arg  SD_TRANSFER_OK: No data transfer is acting
+ *            @arg  SD_TRANSFER_BUSY: Data transfer is acting
+ */
+uint8_t SD_DMA_WritePending(void)
+{
+    return SD_DMA_WritePendingState;
+}
+
+/**
+  * @brief Rx Transfer completed callbacks
+  * @param hsd Pointer SD handle
+  * @retval None
+  */
+void HAL_SD_RxCpltCallback(SD_HandleTypeDef *hsd)
+{
+    SD_DMA_ReadPendingState = SD_TRANSFER_OK;
+}
+
+/**
+  * @brief Tx Transfer completed callbacks
+  * @param hsd Pointer to SD handle
+  * @retval None
+  */
+void HAL_SD_TxCpltCallback(SD_HandleTypeDef *hsd)
+{
+    SD_DMA_WritePendingState = SD_TRANSFER_OK;
+}
+
+/**
+  * @brief SD Abort callbacks
+  * @param hsd: SD handle
+  * @retval None
+  */
+void HAL_SD_AbortCallback(SD_HandleTypeDef *hsd)
+{
+      //BSP_SD_AbortCallback();
+}
+
+void HAL_SD_ErrorCallback(SD_HandleTypeDef *hsd)
+{
+    printf("SD Error\n");
+}
+
+/**
+  * @brief Configure the DMA to receive data from the SD card
+  * @retval
+  *  HAL_ERROR or HAL_OK
+  */
+static HAL_StatusTypeDef SD_DMAConfigRx(SD_HandleTypeDef *hsd)
+{
+  static DMA_HandleTypeDef hdma_rx;
+  HAL_StatusTypeDef status = HAL_ERROR;
+
+  /* Configure DMA Rx parameters */
+  hdma_rx.Init.Request             = DMA_REQUEST_7;
+  hdma_rx.Init.Direction           = DMA_PERIPH_TO_MEMORY;
+  hdma_rx.Init.PeriphInc           = DMA_PINC_DISABLE;
+  hdma_rx.Init.MemInc              = DMA_MINC_ENABLE;
+  hdma_rx.Init.PeriphDataAlignment = DMA_PDATAALIGN_WORD;
+  hdma_rx.Init.MemDataAlignment    = DMA_MDATAALIGN_WORD;
+  hdma_rx.Init.Priority            = DMA_PRIORITY_VERY_HIGH;
+
+  hdma_rx.Instance = DMA2_Channel5;
+
+  /* Associate the DMA handle */
+  __HAL_LINKDMA(hsd, hdmarx, hdma_rx);
+
+  /* Stop any ongoing transfer and reset the state*/
+  HAL_DMA_Abort(&hdma_rx);
+
+  /* Deinitialize the Channel for new transfer */
+  HAL_DMA_DeInit(&hdma_rx);
+
+  /* Configure the DMA Channel */
+  status = HAL_DMA_Init(&hdma_rx);
+
+  /* NVIC configuration for DMA transfer complete interrupt */
+  HAL_NVIC_SetPriority(DMA2_Channel5_IRQn, 6, 0);
+  HAL_NVIC_EnableIRQ(DMA2_Channel5_IRQn);
+
+  return (status);
+}
+
+/**
+  * @brief Configure the DMA to transmit data to the SD card
+  * @retval
+  *  HAL_ERROR or HAL_OK
+  */
+static HAL_StatusTypeDef SD_DMAConfigTx(SD_HandleTypeDef *hsd)
+{
+  static DMA_HandleTypeDef hdma_tx;
+  HAL_StatusTypeDef status;
+
+  /* Configure DMA Tx parameters */
+  hdma_tx.Init.Request             = DMA_REQUEST_7;
+  hdma_tx.Init.Direction           = DMA_MEMORY_TO_PERIPH;
+  hdma_tx.Init.PeriphInc           = DMA_PINC_DISABLE;
+  hdma_tx.Init.MemInc              = DMA_MINC_ENABLE;
+  hdma_tx.Init.PeriphDataAlignment = DMA_PDATAALIGN_WORD;
+  hdma_tx.Init.MemDataAlignment    = DMA_MDATAALIGN_WORD;
+  hdma_tx.Init.Priority            = DMA_PRIORITY_VERY_HIGH;
+
+  hdma_tx.Instance = DMA2_Channel5;
+
+  /* Associate the DMA handle */
+  __HAL_LINKDMA(hsd, hdmatx, hdma_tx);
+
+  /* Stop any ongoing transfer and reset the state*/
+  HAL_DMA_Abort(&hdma_tx);
+
+  /* Deinitialize the Channel for new transfer */
+  HAL_DMA_DeInit(&hdma_tx);
+
+  /* Configure the DMA Channel */
+  status = HAL_DMA_Init(&hdma_tx);
+
+  /* NVIC configuration for DMA transfer complete interrupt */
+  HAL_NVIC_SetPriority(DMA2_Channel5_IRQn, 6, 0);
+  HAL_NVIC_EnableIRQ(DMA2_Channel5_IRQn);
+
+  return (status);
+}
